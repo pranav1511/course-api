bf2934024c4b6944574e4320e3026449a4964703
Pranav Singh <pranav.singh1511@gmail.com>
2019-07-26 15:23:27 +0530

    Added Topic Package

---
+++ src/main/java/com/example/courseapi/topic/TopicService.java
@@ -0,0 +1,33 @@
+package com.example.courseapi.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    public List<Topic> getAllTopics() {
+        return topicRepository.findAll();
+    }
+
+    public Topic getTopic(String id) {
+        return topicRepository.findById(id).orElse(null);
+    }
+
+    public void addTopic(Topic topic) {
+        topicRepository.save(topic);
+    }
+
+    public void updateTopic(Topic topic, String id) {
+        topicRepository.save(topic);
+    }
+
+    public void deleteTopic(String id) {
+        topicRepository.deleteById(id);
+    }
+}
